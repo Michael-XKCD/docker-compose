255e78b3be1ba7ed98d36c2df4ee229979467603
Michael McMurray <mmcmurray@laika.com>
2023-08-05 15:15:43 -0700

    Fixing script

--- nextcloud/change-perms.sh
+++ nextcloud/change-perms.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 data_dir="/data"
+sleep_time="10"
 script="change-perms.sh"
 
-echo "Waiting to $data_dir to mount..."
-until [ ! -d $data_dir ]; do
-    sleep 1
-done
-echo "$data_dir has mounted."
+echo "Waiting $sleep_time secs for $data_dir to mount..."
+sleep $sleep_time
+echo "$sleep_time secs is over."
 
 echo "Executing $script on $data_dir..."
 chown -R :www-data $data_dir
